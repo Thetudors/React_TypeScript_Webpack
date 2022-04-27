16f8c97c50ce59c5d90b1b56c7fef40c99f3cc16
onat <onat@spinmatic.com>
2022-04-27 19:41:41 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './styles.css'
+import IMAGE from './father.png'
+
+export const App = () => {
+    return <> 
+    <h1>React TypeScript Webpack Starter Templates1</h1>
+    <img src={IMAGE} alt="Test Image" width="300" height="300" />
+    </>
+}
