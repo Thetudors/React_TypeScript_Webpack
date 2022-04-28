eee3b7bd0d3bd908f86bfc059a8c7143e2602ff5
onat <onat@spinmatic.com>
2022-04-28 18:14:09 +0300

    added project files and npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './styles.css'
 import IMAGE from './father.png'
+import { ClickCounter } from './ClickCounter'
+import  Button from "@mui/material/Button"
 
 export const App = () => {
     return <> 
-    <h1>React TypeScript Webpack Starter Templates</h1>
-    <img src={IMAGE} alt="Test Image" width="300" height="300" />
+    <h1>test React TypeScript Webpack Starter Templates - {process.env.NODE_ENV} {process.env.name}</h1>
+    <img src={IMAGE} alt="deneme" width="300" height="300" />
+    <ClickCounter/>
+    <Button variant="contained">Hello World</Button>
     </>
 }
